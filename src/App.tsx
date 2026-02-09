de8d3971a9333a27b137fa53e77bf11c8bf39651
atifalinedian <89172863+atifalinedian@users.noreply.github.com>
2026-02-09 20:54:38 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/spun-sugar-showcase">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
